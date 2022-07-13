0ab30fdb1566fd7d8f9dbacb7a34b8dd7df72c21
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-07-13 22:28:45 +0100

    Initial code for tweeting (#10)

--- assets/js/my-library.js
+++ assets/js/my-library.js
@@ -1,8 +1,13 @@
 var serviceData = []
+var submitButton = document.getElementById('btn-search')
 var errorMessage = document.getElementById('search-error')
+var serviceHeader = document.getElementById('h-library-service')
 var fineInfo = document.getElementById('p-fine-info')
 var fineExample = document.getElementById('p-fine-example')
-var serviceHeader = document.getElementById('h-library-service')
+var tweetIntroText = document.getElementById('p-tweet-text')
+var tweetLink = document.getElementById('a-tweet')
+tweetLink.addEventListener('click', tweetLibraryService)
+
 var grid = null
 
 function submitPostcode () {
@@ -18,8 +23,11 @@ function submitPostcode () {
       .then(service_data => {
         if (Object.keys(service_data).length > 0) {
           serviceHeader.innerText = ``
+          serviceHeader.style.display = 'none'
           fineInfo.innerHTML = ``
+          fineInfo.style.display = 'none'
           fineExample.innerHTML = ``
+          fineExample.style.display = 'none'
           var serviceCode = service_data['library_service']
           var service = serviceData.find(
             service => service['Code'] === serviceCode
@@ -38,7 +46,9 @@ function submitPostcode () {
               interval
             )
             serviceHeader.innerText = serviceName
+            serviceHeader.style.display = 'block'
 
+            fineInfo.style.display = 'block'
             if (estimateFamilyWeeklyFine.total > 0) {
               fineInfo.innerHTML = `${serviceName} charge <strong>${formattedFines.child}</strong> fines for children and <strong>${formattedFines.adult}</strong> fines for adults.`
               fineExample.innerHTML = `A family of 4 would pay <strong>£${estimateFamilyWeeklyFine.total.toFixed(
@@ -46,11 +56,30 @@ function submitPostcode () {
               )}</strong> if they were overdue by a week. That could cover the cost of ${
                 estimateFamilyWeeklyFine.example.itemDescription
               }`
+              fineExample.style.display = 'block'
             } else if (estimateFamilyWeeklyFine.total == 0) {
-              fineInfo.innerHTML = `${serviceName} are a fine-free library service.`
+              fineInfo.innerHTML = `${serviceName} do not charge overdue fines.`
             } else {
               fineInfo.innerHTML = `No current info on ${serviceName}`
             }
+          }
+
+          var twitterHandle = service['Twitter handle']
+          if (
+            twitterHandle &&
+            twitterHandle.length > 0 &&
+            estimateFamilyWeeklyFine
+          ) {
+            // Tweet button code
+            var tweetText = fineFree.getTweetText(
+              estimateFamilyWeeklyFine.total
+            )
+            tweetIntroText.innerText = tweetText.tweetIntroduction
+            tweetIntroText.style.display = 'block'
+
+            tweetLink.dataset.related = service['Twitter handle']
+            tweetLink.dataset.text = tweetText.tweetText
+            tweetLink.style.display = 'inline'
           }
         } else {
           errorMessage.innerHTML =
@@ -67,10 +96,19 @@ function submitPostcode () {
   }
 }
 
+function tweetLibraryService (e) {
+  e.preventDefault()
+  var tweetText = `${tweetLink.dataset.text}\n\n@${tweetLink.dataset.related}`
+  var url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    tweetText
+  )} via https://www.finefreelibraries.co.uk`
+  window.open(url, '_blank')
+  return false
+}
+
 fetch(fineFree.services)
   .then(response => response.json())
   .then(services_data => {
-    var submitButton = document.getElementById('btn-search')
     submitButton.addEventListener('click', submitPostcode)
     submitButton.disabled = false
     serviceData = services_data
