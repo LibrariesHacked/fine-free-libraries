b995351d27611a5397136de0ddf2d570dee11844
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-11-04 14:49:29 +0000

    Adding code for displaying range of colours per fine

--- assets/js/library-hex-map.js
+++ assets/js/library-hex-map.js
@@ -4,13 +4,33 @@ const fetchRegionData = fetch(fineFree.regions).then(res => res.json())
 const allData = Promise.all([fetchHexJson, fetchServicesData, fetchRegionData])
 const hexMapElement = document.getElementById('div-library-hexmap')
 
+const colours = ['#fee5d9', '#fcae91', '#fb6a4a', '#de2d26', '#a50f15']
+
 let storedData = null
 
-const buildHexMap = (region = '', childOnly = false) => {
+const buildHexMap = (region = '', childOnly = false, displayCost = false) => {
   hexMapElement.innerHTML = ''
   var hexdata = JSON.parse(JSON.stringify(storedData[0]))
   var serviceData = storedData[1]
   var regions = storedData[2]
+
+  var minFine = 0
+  var maxFine = 0
+
+  serviceData.forEach(service => {
+    // Set the min and max fine values
+    var child = service['Child fine']
+    var adult = service['Adult fine']
+    var fine = childOnly ? child : adult
+    var interval = service['Fine interval']
+    var fineNumber = parseFloat(fine)
+    if (!isNaN(fineNumber)) {
+      // If the interval is per week divide the amount by 7
+      if (interval === 'Week') fineNumber = fineNumber / 7
+      if (fineNumber < minFine) minFine = fineNumber
+      if (fineNumber > maxFine) maxFine = fineNumber
+    }
+  })
 
   Object.keys(hexdata.hexes).forEach(hexCode => {
     var service = serviceData.find(x => x.Code === hexCode)
@@ -32,7 +52,27 @@ const buildHexMap = (region = '', childOnly = false) => {
       hexdata.hexes[hexCode].child = child
       hexdata.hexes[hexCode].adult = adult
       hexdata.hexes[hexCode].interval = interval
-      hexdata.hexes[hexCode].colour = fineFreeLibrary ? '#a5d6a7' : '#eceff1'
+
+      // If displayCost is true use the colour scale to assign a colour to the hex
+      // Display either by adult fine or child fine
+      if (displayCost) {
+        var fine = childOnly ? child : adult
+        var interval = service['Fine interval']
+        var fineNumber = parseFloat(fine)
+        if (!isNaN(fineNumber)) {
+          // If the interval is per week divide the amount by 7
+          if (interval === 'Week') fineNumber = fineNumber / 7
+          // Get a value of 1-5 for the colour scale depending on the fineNumber and the min/max linear scale
+          var colourIndex = Math.round(
+            ((fineNumber - minFine) / (maxFine - minFine)) * 4
+          )
+          hexdata.hexes[hexCode].colour = fineFreeLibrary
+            ? '#a5d6a7'
+            : colours[colourIndex - 1]
+        }
+      } else {
+        hexdata.hexes[hexCode].colour = fineFreeLibrary ? '#a5d6a7' : '#eceff1'
+      }
     }
   })
   new OI.hexmap(hexMapElement, {
@@ -86,18 +126,23 @@ const buildHexMap = (region = '', childOnly = false) => {
 allData.then(res => {
   storedData = res
 
-  // Add option to show Child Only Fines
   var childOnly = document.getElementById('chb-childonly')
+  var displayCost = document.getElementById('chb-cost')
   var region = document.getElementById('sel-region')
   var regionCode = region.value
   var childOnlyChecked = childOnly.checked
+  var displayCostChecked = displayCost.checked
   childOnly.addEventListener('change', function () {
     childOnlyChecked = childOnly.checked
-    buildHexMap(regionCode, childOnlyChecked)
+    buildHexMap(regionCode, childOnlyChecked, displayCostChecked)
+  })
+  displayCost.addEventListener('change', function () {
+    displayCostChecked = displayCost.checked
+    buildHexMap(regionCode, childOnlyChecked, displayCostChecked)
   })
   region.addEventListener('change', function () {
     regionCode = region.value
-    buildHexMap(regionCode, childOnlyChecked)
+    buildHexMap(regionCode, childOnlyChecked, displayCostChecked)
   })
   var regionNames = []
   Object.keys(storedData[2]).forEach(regionCode => {
